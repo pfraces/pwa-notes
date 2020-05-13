61470d1942c02c35f2b8d9ae4f1ab37cfad08a3b
Pau Fracés <pau.fraces.de.blas@everis.com>
2020-05-13 20:48:07 +0200

    added layout improvements

---
+++ src/components/Board.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <md-card md-with-hover v-for="(card, index) in cards" :key="index">
+      <md-card-header>
+        <div class="md-title">{{ card.title }}</div>
+      </md-card-header>
+
+      <md-card-content>{{ card.content }}</md-card-content>
+
+      <md-card-actions>
+        <md-button>Edit</md-button>
+        <md-button>Delete</md-button>
+      </md-card-actions>
+    </md-card>
+  </div>
+</template>
+
+<style scoped>
+.md-card {
+  width: 320px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
+
+<script>
+const loremIpsum =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.';
+
+export default {
+  name: 'App',
+  components: {},
+  data: function() {
+    return {
+      cards: [
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum },
+        { title: 'Lorem Ipsum', content: loremIpsum }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/Layout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-primary">
+        <h3 class="md-title" style="flex: 1">PWA Notes</h3>
+        <md-button class="md-primary" @click="showDialog = true">Create</md-button>
+      </md-app-toolbar>
+
+      <md-app-content>
+        <Board />
+      </md-app-content>
+    </md-app>
+
+    <md-dialog :md-active.sync="showDialog">
+      <md-field>
+        <md-input placeholder="Title" v-model="newCard.title"></md-input>
+      </md-field>
+
+      <md-field>
+        <md-textarea placeholder="Content" v-model="newCard.content"></md-textarea>
+      </md-field>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showDialog = false">Cancel</md-button>
+        <md-button class="md-primary" @click="showDialog = false">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </div>
+</template>
+
+<style scoped>
+.md-app {
+  max-height: 100vh;
+  border: 1px solid rgba(#000, 0.12);
+}
+
+.md-dialog /deep/ .md-dialog-container {
+  transform: none;
+  width: 370px;
+  padding: 10px;
+}
+
+.md-dialog /deep/ .md-textarea {
+  height: 160px;
+}
+</style>
+
+<script>
+import Board from './Board.vue';
+
+export default {
+  name: 'Layout',
+  components: {
+    Board
+  },
+  data: function() {
+    return {
+      showDialog: false,
+      newCard: {
+        title: '',
+        content: ''
+      }
+    };
+  }
+};
+</script>
