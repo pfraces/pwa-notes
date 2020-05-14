598ec33faf70a53adb0b55c7356a46362af7837c
Pau Fracés <pau.fraces.de.blas@everis.com>
2020-05-14 21:08:10 +0200

    added feature to retrieve server data

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,9 +2,31 @@
   <div>
     <md-app md-waterfall md-mode="fixed">
       <md-app-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="menuVisible = true">
+          <md-icon>menu</md-icon>
+        </md-button>
         <h3 class="md-title" style="flex: 1">PWA Notes</h3>
         <md-button class="md-primary" @click="createNote()">Create</md-button>
       </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar md-elevation="0">
+          <h3>Admin</h3>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="menuVisible = false">
+              <md-icon>keyboard_arrow_left</md-icon>
+            </md-button>
+          </div>
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item @click="retrieveServerData()">
+            <md-icon class="md-accent">refresh</md-icon>
+            <h4 class="md-list-item-text">Sync</h4>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
 
       <md-app-content>
         <Board v-bind:cards="notes" v-on:edit-note="editNote" v-on:delete-note="deleteNote" />
@@ -23,6 +45,10 @@
   height: 100vh;
   border: 1px solid rgba(#000, 0.12);
 }
+
+.md-drawer {
+  width: 250px;
+}
 </style>
 
 <script>
@@ -33,7 +59,7 @@ import * as axios from 'axios';
 
 var db = new PouchDB('notes');
 
-const retrieveData = function(callback) {
+const retrieveLocalData = function(callback) {
   db.allDocs({ include_docs: true, descending: true }, (err, doc) => {
     if (err) {
       throw err;
@@ -41,6 +67,12 @@ const retrieveData = function(callback) {
 
     const data = doc.rows.map(row => row.doc);
     callback(data);
+  });
+};
+
+const retrieveServerData = function() {
+  axios.get('http://localhost:3000/api/notes').then(function(res) {
+    db.bulkDocs(res.data);
   });
 };
 
@@ -57,7 +89,7 @@ const serverSync = function({ deleted, doc, changes }) {
   }
 
   if (rev === 1) {
-    axios.post(`http://localhost:3000/api/notes`, {
+    axios.post('http://localhost:3000/api/notes', {
       _id: doc._id,
       title: doc.title,
       content: doc.content
@@ -88,18 +120,19 @@ export default {
       live: true,
       include_docs: true
     }).on('change', change => {
-      retrieveData(data => {
+      retrieveLocalData(data => {
         this.notes = data;
         serverSync(change);
       });
     });
 
-    retrieveData(data => {
+    retrieveLocalData(data => {
       this.notes = data;
     });
   },
   data: function() {
     return {
+      menuVisible: false,
       showNoteDialog: false,
       note: null,
       notes: []
@@ -119,6 +152,10 @@ export default {
     },
     deleteNote(event) {
       db.remove(event);
+    },
+    retrieveServerData() {
+      retrieveServerData();
+      this.menuVisible = false;
     }
   }
 };
