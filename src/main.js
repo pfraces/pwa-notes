0c9afff34e0a7c8f723c93fb3b6e33284939ba21
Pau Fracés <pau.fraces.de.blas@everis.com>
2020-05-14 10:26:52 +0200

    added vue-pouchdb integration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue';
+
+import PouchVue from 'pouch-vue';
+import PouchDB from 'pouchdb-browser';
+import PouchFind from 'pouchdb-find';
+import PouchLiveFind from 'pouchdb-live-find';
+
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
 import App from './App.vue';
+
+PouchDB.plugin(PouchFind);
+PouchDB.plugin(PouchLiveFind);
+
+Vue.use(PouchVue, {
+  pouch: PouchDB,
+  defaultDB: 'pwa-notes'
+});
 
 Vue.use(VueMaterial);
 
